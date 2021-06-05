394d0a8053f06cbff7a6ea20211d8b480b3be400
fydne <serverafydne@gmail.com>
2021-06-05 23:29:20 +0500

    1.4.3

---
+++ Qurre/API/Audio.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+namespace Qurre.API
+{
+    public static class Audio
+    {
+        public static void Play(Stream stream, float volume);
+        public static void PlayFromFile(string path, float volume);
+        public static void PlayFromUrl(string url, float volume);
+    }
+}
