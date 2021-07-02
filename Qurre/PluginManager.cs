d953b4e66055d563ef90f64a50215e2c676f6267
fydne <serverafydne@gmail.com>
2021-07-03 02:38:02 +0500

    1.5.2

--- Qurre/PluginManager.cs
+++ Qurre/PluginManager.cs
@@ -10,8 +10,6 @@ namespace Qurre
         public static string PluginsDirectory { get; }
         public static string QurreDirectory { get; }
         public static string AppDataDirectory { get; }
-        public static int Planid { get; }
-        public static string Plan { get; }
         public static Version Version { get; }
         public static string ManagedAssembliesDirectory { get; }
         public static string ConfigsPath { get; }
@@ -19,7 +17,6 @@ namespace Qurre
         public static void Disable();
         public static void Enable();
         public static void LoadPlugin(string mod);
-        public static IEnumerator<float> LoadPlugins();
         public static void Reload();
         public static void ReloadPlugins();
     }
