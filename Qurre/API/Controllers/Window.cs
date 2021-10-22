f6fc4eb70e49ba1e9b7e89783b58fce587cacd8e
fydne <serverafydne@gmail.com>
2021-10-22 19:56:53 +0500

    1.9.2

--- Qurre/API/Controllers/Window.cs
+++ Qurre/API/Controllers/Window.cs
@@ -3,9 +3,12 @@
     public class Window
     {
         public BreakableWindow Breakable { get; }
+        public string Name { get; set; }
         public global::UnityEngine.GameObject GameObject { get; }
         public global::UnityEngine.Transform Transform { get; }
-        public global::UnityEngine.Vector3 Position { get; }
+        public global::UnityEngine.Vector3 Position { get; set; }
+        public global::UnityEngine.Quaternion Rotation { get; set; }
+        public global::UnityEngine.Vector3 Scale { get; set; }
         public global::UnityEngine.Vector3 Size { get; }
         public global::Footprinting.Footprint LastAttacker { get; }
         public bool Destroyed { get; set; }
